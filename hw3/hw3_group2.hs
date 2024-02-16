7d63a95359061acf9781c37a1c086abd6173a0bc
Matthew Pacey <97149606+osu-mp@users.noreply.github.com>
2024-02-15 21:52:35 -0800

    Completed stack language, testing included (#12)
    * Completed stack language, testing included
    
    * Implemented Caitlyn's feedback to simplify ADD/SWAP

--- hw3/hw3_group2.hs
+++ hw3/hw3_group2.hs
@@ -1,3 +1,37 @@
 module Hw3_group2 where
 
--- TODO
+-- 3 Stack language
+
+-- abstract syntax and supported operations
+type S = [Op]
+data Op = LD Int | ADD | SWAP | DUP
+
+-- stack is a list of integers
+type Stack = [Int]
+
+-- result of operation can be either a stack or a failure string
+data Result a = Success a | Fail String
+semOp :: Op -> Stack -> Result Stack
+
+-- load given int to top of stack
+semOp (LD x) stack = Success (x : stack)
+
+-- add the first 2 elements on the stack or error if fewer than 2 elements present
+semOp ADD (x:y:stack) = Success (x + y : stack)
+semOp ADD _ = Fail ("Error: ADD requires at least 2 elements on stack") 
+
+-- swap first 2 elements or produce error if fewer than 2 elements
+semOp SWAP (x:y:stack) = Success (y : x: stack)
+semOp SWAP _ = Fail ("Error: SWAP requires at least 2 elements on stack")
+
+-- duplicate top element and push onto stack
+-- if stack is empty, produce error
+semOp DUP [] = Fail ("Error: DUP requires at least 1 element on stack")
+semOp DUP (x:xs) = Success (x:x:xs)
+
+semS :: Stack -> [Op] -> Result Stack
+semS stack [] = Success stack
+semS stack (op:ops) = 
+    case semOp op stack of
+        Success newStack -> semS newStack ops
+        Fail err         -> Fail err
